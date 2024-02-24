99a52b90b8f196745e4f6df2fc20596825b7c275
Juan Hernández <janukato@gmail.com>
2024-02-24 14:01:02 +0000

    first triangle

--- main.cpp
+++ main.cpp
@@ -1,42 +1,46 @@
 #include <GL/glew.h>
 #include <GL/glu.h>
-#include <stdio.h>
+#include <iostream>
 #include <string>
 
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL.h>
 
+using std::cout;
+using std::endl;
+
+bool processInput();
+void createShaders();
+
 const char* TITLE = "LearnOpenGL";
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
 
-bool processInput() {
-    SDL_Event e;
-    bool quit = false;
+GLuint shaderProgram;
 
-    while(SDL_PollEvent(&e)) {
-        switch (e.type) {
-            case SDL_KEYDOWN:
-                if (e.key.keysym.sym != SDLK_ESCAPE) {
-                    break;
-                }
-            case SDL_QUIT:
-                quit = true;
-                break;
-        }
-    }
 
-    return quit;
-}
 
-int main(int argc, char **argv) {
+int main()
+{
     GLint width = 800;
     GLint height = 600;
     SDL_Window* window = nullptr;
     SDL_GLContext context = nullptr;
-    GLuint gProgramID = 0;
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        cout << "SDL could not initialize! SDL_Error:" << SDL_GetError() << endl;
         return 1;
     }
 
@@ -54,42 +58,69 @@ int main(int argc, char **argv) {
         SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
     );
 
-    if (window == nullptr) {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    if (window == nullptr)
+    {
+        cout << "Window could not be created! SDL_Error:" << SDL_GetError() << endl;
         return 1;
     }
 
     context = SDL_GL_CreateContext(window);
 
-    if (context == nullptr) {
-        printf("OpenGL context could not be created! SDL Error: %s\n", SDL_GetError());
+    if (context == nullptr)
+    {
+        cout << "OpenGL context could not be created! SDL Error:" << SDL_GetError() << endl;
         return 1;
     }
 
     glewExperimental = GL_TRUE;
     GLenum glewError = glewInit();
-    glViewport(0, 0, width, height);
 
     if (glewError != GLEW_OK){
-        printf("Error initializing GLEW! %s\n", glewGetErrorString(glewError));
+        cout << "Error initializing GLEW!" << glewGetErrorString(glewError) << endl;
         return 1;
     }
 
-    if (SDL_GL_SetSwapInterval(1) < 0) {
-        printf("Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError());
+    if (SDL_GL_SetSwapInterval(1) < 0)
+    {
+        cout << "Warning: Unable to set VSync! SDL Error:" << SDL_GetError() << endl;
         return 1;
     }
-/*
-    gProgramID = glCreateProgram();
 
-    if (gProgramID == 0) {
-        printf("Unable to initialize OpenGL!\n");
-        return 1;
-    }*/
+    createShaders();
 
-    while (!processInput()) {
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+    GLuint VBO, VAO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind VBO, allowed but not necessary in this case
+    glBindVertexArray(0); // unbind VAO, allowed but not necessary in this case
+
+    // uncomment this call to draw in wireframe polygons.
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    while (!processInput())
+    {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         SDL_GL_SwapWindow(window);
     }
 
@@ -97,4 +128,66 @@ int main(int argc, char **argv) {
     SDL_Quit();
 
     return 0;
+}
+
+bool processInput()
+{
+    SDL_Event e;
+    bool quit = false;
+
+    while(SDL_PollEvent(&e))
+    {
+        switch (e.type)
+        {
+            case SDL_KEYDOWN:
+                if (e.key.keysym.sym != SDLK_ESCAPE)
+                {
+                    break;
+                }
+            case SDL_QUIT:
+                quit = true;
+                break;
+        }
+    }
+
+    return quit;
+}
+
+void createShaders()
+{
+    constexpr uint logSize = 512;
+    int success;
+    char infoLog[logSize];
+    shaderProgram = glCreateProgram();
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, logSize, nullptr, infoLog);
+        cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED:" << infoLog << endl;
+    }
+
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, logSize, nullptr, infoLog);
+        cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED:" << infoLog << endl;
+    }
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 }
