c3c0e6785f1f83be6d3741b35ed7d480c6430691
Juan Hernández <janukato@gmail.com>
2024-03-09 12:11:22 +0000

    transformations

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "src/shader.h"
 #include "src/image.h"
 
@@ -73,6 +77,7 @@ int main()
     }
 
     Shader shader("../shaders/tex_shader.vs", "../shaders/tex_shader.fs");
+    Shader shader2("../shaders/tex_shader.vs", "../shaders/tex_shader.fs");
 
     float vertices[] = {
         // positions          // colors           // texture coords
@@ -87,18 +92,18 @@ int main()
     };
 
     // Create buffers
-    GLuint VBO, VAO, EBO;
+    GLuint VBO[2], VAO[2], EBO[2];
 
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+    glGenVertexArrays(2, VAO);
+    glGenBuffers(2, VBO);
+    glGenBuffers(2, EBO);
 
-    glBindVertexArray(VAO);
+    glBindVertexArray(VAO[0]);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -110,11 +115,28 @@ int main()
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
-    GLuint texture1, texture2;
-    int texWidth, texHeight, nrChannels;
-    glGenTextures(1, &texture1);
+    glBindVertexArray(VAO[1]);
 
-    glBindTexture(GL_TEXTURE_2D, texture1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    GLuint textures[2];
+    int texWidth, texHeight, nrChannels;
+    glGenTextures(2, textures);
+
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -134,8 +156,7 @@ int main()
 
     stbi_image_free(data);
 
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -159,8 +180,23 @@ int main()
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     shader.use();
     glUniform1i(glGetUniformLocation(shader.ID, "texture1"), 0);
-    shader.setInt("texture2", 1);
-    shader.setFloat("mixPercentage", 0.2);
+    glUniform1i(glGetUniformLocation(shader.ID, "texture2"), 1);
+    shader.setFloat("mixPercentage", 0.2f);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+
+    shader2.use();
+    shader2.setInt("texture1", 0);
+    shader2.setInt("texture2", 1);
+    shader2.setFloat("mixPercentage", 0.2f);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
 
     while (!processInput(shader))
     {
@@ -168,24 +204,40 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Uint64 ticks = SDL_GetTicks64();
+        Uint64 ticks = SDL_GetTicks64();
+
+
         // float offset = sin(ticks / 1000.0f) / 2.0f;
         // GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
         // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         // shader.setFloat("offset", offset);
 
         // bind textures
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-
         shader.use();
-        glBindVertexArray(VAO);
+
+        glm::mat4 transform = glm::mat4(1.0f);
+        GLuint transformLoc = glGetUniformLocation(shader.ID, "transform");
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        transform = glm::rotate(transform, static_cast<float>(ticks / 1000.0f) + 2.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        glBindVertexArray(VAO[0]);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        // glBindVertexArray(VAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        shader2.use();
+
+        glm::mat4 transform2 = glm::mat4(1.0f);
+        GLuint transformLoc2 = glGetUniformLocation(shader2.ID, "transform");
+        transform2 = glm::translate(transform2, glm::vec3(-0.5f, 0.5f, 0.0f));
+        transform2 = glm::scale(transform2, glm::vec3(
+            abs(sin(static_cast<float>(ticks / 1000.0f))),
+            abs(sin(static_cast<float>(ticks / 1000.0f))),
+            0.0f)
+        );
+        glUniformMatrix4fv(transformLoc2, 1, GL_FALSE, glm::value_ptr(transform2));
+
+        glBindVertexArray(VAO[1]);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         SDL_GL_SwapWindow(window);
     }
